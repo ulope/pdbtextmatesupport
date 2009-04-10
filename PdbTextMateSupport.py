5fe69acfcf0714c5dc1a882cdf2a0d693e67806f
Ulrich Petri <mail@ulo.pe>
2009-04-11 01:52:15 +0200

    add appscript support

--- PdbTextMateSupport.py
+++ PdbTextMateSupport.py
@@ -1,13 +1,23 @@
 from os.path import exists
 from os import system
 
+have_appscript = False
+try:
+    from appscript import app
+    have_appscript = True
+except:
+    pass
+
 def mate(self):
     frame, lineno = self.stack[self.curindex]
     filename = self.canonic(frame.f_code.co_filename)
     if exists(filename):
         tm_url = 'txmt://open?url=file://%s&line=%d&column=2' % (filename, lineno)
-        osa_cmd = 'tell application "TextMate" to get url "%s"' % tm_url
-        system('osascript -e \'%s\'' % osa_cmd)
+        if have_appscript:
+            app("TextMate").get_url(tm_url)
+        else:
+            osa_cmd = 'tell application "TextMate" to get url "%s"' % tm_url
+            system('osascript -e \'%s\'' % osa_cmd)
 
 def preloop(self):
     mate(self)
